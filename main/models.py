fb14b5eac5c5ee519c64c0c38ef58192f460c733
Antinormanist <katya.goryacheva.2009@gmail.com>
2024-11-16 07:46:54 +1000

    Add Task and Template models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+from user.models import User
+
 # Create your models here.
+Priority_choice = (
+    ('common', 'common'),
+    ('simple', 'simple'),
+    ('important', 'important'),
+    ('strong', 'strong')
+)
+
+class Task(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(max_length=256, null=True)
+    minutes = models.PositiveIntegerField(null=True)
+    priority = models.CharField(choices=Priority_choice, max_length=9, default='common')
+    remind = models.DateField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    template = models.ForeignKey(to='Template', on_delete=models.CASCADE, null=True)
+    is_templated = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'tasks'
+
+
+class Template(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(max_length=256, null=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'templates'
